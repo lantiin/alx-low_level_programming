e3a584d5d11533506f2498f73171876e77b5f04d
lantiin <akwesimemsah8888@gmail.com>
2022-10-19 09:37:02 -0700

    d

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,13 +6,13 @@
  */
 int print_last_digit(int n)
 {
-	int x = n % 10;
+	int last_digit = n % 10;
 
 		if (n < 0)
 		{
-			x = x * -1;
+			last_digit = last_digit * -1;
 		}
-		_putchar (x);
-		return (X);
+		_putchar (last_digit + '0');
+		return (last_digit);
 }
 
