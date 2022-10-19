d7b406fa8f26e750d7a38e0de6eca0da6db5fd71
lantiin <akwesimemsah8888@gmail.com>
2022-10-19 10:04:20 -0700

    d

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,11 +8,11 @@ void jack_bauer(void)
 	int hour;
 	int minute;
 
-	for (hours = 0; hours <= 23; hours++)
+	for (hour = 0; hour <= 23; hour++)
 	{
 		for (minute = 0; minute <= 59; minute++)
-			_putchar((hours / 10) + '0');
-			_putchar((hours % 10) + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');
