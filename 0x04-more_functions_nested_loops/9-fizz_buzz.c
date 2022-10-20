6a0f82cec117763b47b62794a64eb0c40d4f0319
lantiin <akwesimemsah8888@gmail.com>
2022-10-20 16:44:40 -0700

    dd

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,9 +11,9 @@ int main(void)
 {
 		if (a % 15 == 0)
 			printf("FizzBuzz");
-		else if (a % 3)
+		else if (a % 3 == 0)
 			printf("Fizz");
-		else if (a % 5)
+		else if (a % 5 == 0)
 			printf("Buzz");
 		else
 			printf("%i", a);
